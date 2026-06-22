df304501a98c4b89df0e63322f1bd3f88289e854
hsthirumalesh <hsthirumalesh@gmail.com>
2026-06-22 21:40:12 +0200

    deploy on gh

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,10 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
-  base: "/OpenSiliconTest/",
-
-  build: {
-    outDir: "dist"
-  }
+  base: "/OpenSiliconTest/"
 });
